dd2c46f6dabd664d89b24537c2ec061fef55682f
seohag <sscgeum@gmail.com>
2024-06-04 12:05:01 +0900

    [2024.06.04 금서하 요세푸스 문제]

---
+++ solutions/요세푸스.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "요세푸스.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const [N, K] = input[0].split(" ").map(Number);
+
+const answer = [];
+const queue = [];
+for (let i = 1; i <= N; i++) {
+  queue.push(i);
+}
+for (let i = 1; i <= N; i++) {
+  for (let j = 1; j <= K; j++) {
+    if (j === K) {
+      answer.push(queue.shift());
+    } else {
+      queue.push(queue.shift());
+    }
+  }
+}
+
+console.log(`<${answer.join(", ")}>`);
