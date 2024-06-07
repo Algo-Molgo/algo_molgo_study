53a6ce71fd607dc1893b396cd05f0da48a333bc9
tjd985 <tjd985@gmail.com>
2024-06-07 11:20:59 +0900

    ✨ 2024.06.07 문제풀이
    백준 ATM

---
+++ solutions/ATM.js
@@ -0,0 +1,24 @@
+const INPUT_PATH = "../inputs/ATM.txt";
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : INPUT_PATH;
+const [peopleLength, people] = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
+let result = 0;
+
+const sortedPeople = people
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+
+sortedPeople.reduce((acc, cur, index) => {
+  acc = acc + cur;
+  result += acc;
+
+  return acc;
+}, 0);
+
+console.log(result);
