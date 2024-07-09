fc98d795874a6db54d8bf3b20d83dd7446c2ec97
sht02048 <sht02048@gmail.com>
2024-07-09 11:43:32 +0900

    Fail: sticker

---
+++ solutions/sticker.js
@@ -0,0 +1,7 @@
+const INPUT_PATH = "inputs/sticker.txt";
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : INPUT_PATH;
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+console.log(input);
