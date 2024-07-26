bfa086ece04fbfe85db3f4a849bd7460d4aa203d
tjd985 <tjd985@gmail.com>
2024-07-26 21:17:21 +0900

    ♻️ [문제 재풀이] 숨바꼭질

--- solutions/숨바꼭질.js
+++ solutions/숨바꼭질.js
@@ -4,41 +4,37 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : INPUT_PATH;
 const input = fs.readFileSync(filePath).toString().trim().split(" ");
 const [startNumber, targetNumber] = input.map(Number);
-let result = Infinity;
 
-function solution(currentNumber, count) {
-  if (currentNumber === targetNumber) {
-    result = Math.min(count, result);
+function solution(currentNumber) {
+  const countList = new Array(100001).fill(0);
+  const queue = [currentNumber];
 
-    return count;
-  }
+  while (queue.length) {
+    const currentNumber = queue.shift();
 
-  if (currentNumber > targetNumber) {
-    return solution(currentNumber - 1, count + 1);
-  }
-
-  const prevNumber = currentNumber - 1;
-  const nextNumber = currentNumber + 1;
-
-  if (currentNumber * 2 > targetNumber) {
-    if (
-      Math.abs(targetNumber - prevNumber * 2) <
-      Math.abs(targetNumber - currentNumber * 2)
-    ) {
-      return solution(prevNumber, count + 1);
+    if (currentNumber === targetNumber) {
+      return countList[currentNumber];
     }
-  }
 
-  if (currentNumber * 2 < targetNumber) {
-    if (
-      Math.abs(targetNumber - nextNumber * 2) <
-      Math.abs(targetNumber - currentNumber * 2)
-    ) {
-      return solution(nextNumber, count + 1);
+    const nextNumbers = [
+      currentNumber - 1,
+      currentNumber + 1,
+      currentNumber * 2,
+    ];
+
+    for (const nextNumber of nextNumbers) {
+      const isSkip =
+        nextNumber < 0 || nextNumber > 100000 || countList[nextNumber] !== 0;
+
+      if (isSkip) {
+        continue;
+      }
+
+      countList[nextNumber] = countList[currentNumber] + 1;
+
+      queue.push(nextNumber);
     }
   }
 }
 
-solution(startNumber, 0);
-
-console.log(result);
+console.log(solution(startNumber));
