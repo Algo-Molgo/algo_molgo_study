16bcf560418ef61407bc6ae06a95f8beb0732880
sht02048 <sht02048@gmail.com>
2024-08-06 12:03:16 +0900

    Fail easyStair

---
+++ solutions/easyStair.js
@@ -0,0 +1,8 @@
+const INPUT_PATH = "inputs/easyStair.txt";
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : INPUT_PATH;
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const N = Number(input[0]);
+
